f5b1528f11f6fd7f505316d796fa9af91a38fce4
ulises <diseno.aruma@gmail.com>
2020-10-20 13:00:12 -0500

    cambios en vista

---
+++ app/src/main/java/mx/gob/cdmx/estudioscdmx/view/FormatoActivity.java
@@ -0,0 +1,16 @@
+package mx.gob.cdmx.estudioscdmx.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mx.gob.cdmx.estudioscdmx.R;
+
+public class FormatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formato);
+    }
+}
