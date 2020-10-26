7fa5c2d5ce5286c46b0969ba60cfaec5241b7685
Ulises <diseno.aruma@gmail.com>
2020-10-26 17:54:21 -0600

    foto vista

---
+++ app/src/main/java/mx/gob/cdmx/estudioscdmx/FotoActivity.java
@@ -0,0 +1,14 @@
+package mx.gob.cdmx.estudioscdmx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto);
+    }
+}
