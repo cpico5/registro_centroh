fa98dfe9ad6f7083720f0c5609f3660c49f3cd14
Luis <luisosva@live.com.mx>
2020-11-10 13:30:20 -0600

    FOTO

--- app/src/main/java/mx/gob/cdmx/estudioscdmx/FotoActivity.java
+++ app/src/main/java/mx/gob/cdmx/estudioscdmx/FotoActivity.java
@@ -3,6 +3,7 @@ package mx.gob.cdmx.estudioscdmx;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
@@ -15,26 +16,41 @@ import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.MySSLSocketFactory;
+import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.RequestParams;
 import com.thecode.aestheticdialogs.AestheticDialog;
 import com.thecode.aestheticdialogs.DialogStyle;
 import com.thecode.aestheticdialogs.DialogType;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+import cz.msebera.android.httpclient.Header;
+import mx.gob.cdmx.estudioscdmx.centroh.LoginActivity;
 import mx.gob.cdmx.estudioscdmx.db.DaoManager;
 import mx.gob.cdmx.estudioscdmx.model.Entrevista;
 import mx.gob.cdmx.estudioscdmx.model.Firma;
 import mx.gob.cdmx.estudioscdmx.model.Foto;
 import mx.gob.cdmx.estudioscdmx.model.Usuario;
+import mx.gob.cdmx.estudioscdmx.service.Imei;
 
 import static android.os.Environment.getExternalStorageDirectory;
 import static mx.gob.cdmx.estudioscdmx.Nombre.ENTREVISTA;
@@ -46,6 +62,7 @@ public class FotoActivity extends AppCompatActivity {
 
     Usuario usuario;
     Entrevista entrevista;
+    Firma firma;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_IMAGE_CODE = 0;
@@ -64,9 +81,17 @@ public class FotoActivity extends AppCompatActivity {
     SQLiteHelper3 sqLiteHelper3;
     SQLiteDatabase sqLiteDatabase;
 
+    private SQLiteDatabase dbs;
+    private UsuariosSQLiteHelper3 usdbhs;
+
     DaoManager daoManager;
 
     String currentPhotoPath;
+
+    Button guardar;
+
+    Foto foto1 = new Foto();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,6 +107,23 @@ public class FotoActivity extends AppCompatActivity {
             public void onClick(View view) {
                 dispatchTakePictureIntent();
 
+            }
+        });
+
+        guardar = findViewById(R.id.guardar);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!foto1.getFotoPath().isEmpty() || foto1.getFotoPath() != null){
+                    guardaws();
+                }else{
+                    new AestheticDialog.Builder(FotoActivity.this, DialogStyle.FLAT, DialogType.WARNING)
+                            .setTitle("Foto")
+                            .setMessage("Debe tomar una foto")
+                            .show();
+                    return;
+                }
             }
         });
 
@@ -158,24 +200,15 @@ public class FotoActivity extends AppCompatActivity {
                 UUID uuid = UUID.randomUUID();
                 convertBitmapToFile(imageScaled, uuid.toString());
 
-
-                Foto foto1 = new Foto();
-
-
-
                 sqLiteHelper3 = new SQLiteHelper3(this);
                 sqLiteDatabase = sqLiteHelper3.getWritableDatabase();
-
                 daoManager = new DaoManager(sqLiteDatabase);
 
                 daoManager.delete(Foto.class);
 
                 foto1.setFotoPath(imagescale);
-
                 foto1.setUuid(uuid);
-
                 GPSTracker gps = new GPSTracker(FotoActivity.this);
-
                 foto1.setLatitude(gps.getLatitude());
                 foto1.setLongitude(gps.getLongitude());
 
@@ -204,6 +237,90 @@ public class FotoActivity extends AppCompatActivity {
 
 
         }
+    }
+
+    private void guardaws(){
+
+
+        usdbhs = new UsuariosSQLiteHelper3(FotoActivity.this);
+        dbs = usdbhs.getWritableDatabase();
+        DaoManager daoManagerUser = new DaoManager(dbs);
+
+
+        usuario = (Usuario) daoManagerUser.findOne(Usuario.class);
+
+        sqLiteHelper3 = new SQLiteHelper3(this);
+        sqLiteDatabase = sqLiteHelper3.getWritableDatabase();
+        daoManager = new DaoManager(sqLiteDatabase);
+
+        entrevista = (Entrevista) daoManager.findOne(Entrevista.class);
+
+        firma = (Firma) daoManager.findOne(Firma.class);
+
+        Imei imei = new Imei(this);
+        String device_info = imei.getDeviceInof();
+        Log.d(TAG, device_info);
+        RequestParams params = new RequestParams();
+        params.put("person", entrevista.getSuscribe());
+        params.put("person_position", entrevista.getCaracter());
+        params.put("street", entrevista.getInmueble());
+        params.put("no_ext", entrevista.getNoOficial());
+        params.put("no_int", entrevista.getNoInterior());
+        params.put("settlement_id", entrevista.getColonia());
+        params.put("municipality_id", entrevista.getAlcaldia());
+        params.put("cp", entrevista.getCp());
+        params.put("account_predial", entrevista.getCuentaPredial());
+        params.put("phone", entrevista.getTelefono());
+
+
+        GPSTracker gps = new GPSTracker(FotoActivity.this);
+        double latitud = gps.getLatitude();
+        double longitud = gps.getLongitude();
+
+        Log.i(TAG, "----------> la latitud: "+ latitud);
+        Log.i(TAG, "----------> la longitud: "+ longitud);
+        params.put("latitude", latitud);
+        params.put("longitude", longitud);
+        AsyncHttpClient client = new AsyncHttpClient();
+
+
+        client.setSSLSocketFactory(MySSLSocketFactory.getFixedSocketFactory());
+        client.addHeader("Authorization", usuario.getToken_type()+usuario.getAccess_token());
+        // client.setBasicAuth("email","password", new AuthScope("example.com", 80, AuthScope.ANY_REALM));
+        // client.get("https://example.com");
+
+        RequestHandle requestHandle = client.post(getResources().getString(R.string.url_aplicacion) + "/api/achpredial/document", params, new AsyncHttpResponseHandler() {
+            String jsonToken = "";
+            int id_user;
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String nombreStr = "";
+                Log.d(TAG, "Realizo la conexión");
+
+                Log.d(TAG, "e2lira -----------> " + new String(responseBody));
+                String json = new String(responseBody);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                try {
+                    Log.e(TAG, "existe un error en la conexión status code: " + statusCode);
+                    String json = new String(responseBody);
+                }catch (Exception e){
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+
     }
 
     private void convertBitmapToFile(Bitmap bitmap, String name) {
